af3dfffb728e0f5ce516a2796fe50a5c4238208d
CodeKiro <kirofiniello@outlook.it>
2024-08-21 16:48:39 +0200

    scaffolding di base con collegamento dei componenti Header Main e footer, all'app

---
+++ vite-boolfolio/src/components/MainFooter.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+<h1>
+    io sono il footer
+</h1>
+</template>
+
+<style scoped>
+
+</style>
