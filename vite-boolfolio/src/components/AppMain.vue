a3f84aad242fe3df07c1e216a322a47cded25db8
CodeKiro <kirofiniello@outlook.it>
2024-08-23 16:40:35 +0200

    ho passato i dati dall'appmain alla project card, e poi ho iterato con un v-for per permettere di vedere tutti i projects

--- vite-boolfolio/src/components/AppMain.vue
+++ vite-boolfolio/src/components/AppMain.vue
@@ -1,7 +1,11 @@
 <script>
 import axios from 'axios';
+import ProjectCard from './ProjectCard.vue';
 
 export default {
+    components : {
+        ProjectCard
+    },
     data() {
         return {
             projects: [],  // Assicurati che sia 'projects'
@@ -32,13 +36,35 @@ export default {
 </script>
 
 <template>
-<ul>
+<!-- <ul>
     <li v-for="project in projects" :key="project.id">
         {{ project.title }}
     </li>
-</ul>
+</ul> -->
+<ProjectCard
+v-for="project in projects"
+:key="project.id"
+:author="project.author"
+:title="project.title"
+:content="project.content"
+:image="project.image"
+/>
+
 </template>
 
 <style scoped>
+body {
+    font-family: 'Arial', sans-serif;
+    background-color: #f5f5f5;
+    color: #333333;
+    margin: 0;
+    padding: 0;
+}
+
+#app {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 20px;
+}
 
 </style>
---
+++ vite-boolfolio/src/components/ProjectCard.vue
@@ -0,0 +1,78 @@
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+        props: {
+    author: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    }
+    }
+}
+</script>
+
+<template>
+    <article class="card">
+        <h1>{{ title }}</h1>
+        <h3>{{ author }}</h3>
+        <img :src="image" alt="Project Image" class="project-image" />
+            <p>{{ content }}</p>
+    </article>
+</template>
+
+<style scoped>
+
+.project-image {
+    max-width: 500px;
+    height: 500px;
+    border-radius: 8px;
+    margin-bottom: 16px;
+}
+.card {
+    background-color: #a6f7de;
+    border: 1px solid #e0e0e0;
+    border-radius: 8px;
+    padding: 16px;
+    margin: 16px;
+    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+
+.card:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 4px 15px rgba(0, 0, 0, 0.2);
+}
+
+h1 {
+    font-size: 1.5em;
+    color: #333333;
+    margin-bottom: 8px;
+}
+
+h3 {
+    font-size: 1em;
+    color: #777777;
+    margin-bottom: 16px;
+}
+
+p {
+    font-size: 0.9em;
+    color: #555555;
+    line-height: 1.6;
+}
+
+</style>
